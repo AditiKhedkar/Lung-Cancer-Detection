478f0e2de6167b7afaae5928762b25733040a8ab
Aditi Khedkar <36340264+ADITIJ700@users.noreply.github.com>
2021-07-09 12:44:52 +0530

    commented and explained code

--- Backend/preprocessing.py
+++ Backend/preprocessing.py
@@ -1,9 +1,9 @@
-import numpy as np
-from scipy.ndimage import median_filter
-from sklearn.cluster import KMeans
-from skimage import morphology,measure
-import matplotlib.pyplot as plt
-def preprocess_image(arr):
+import numpy as np  #array processing,functions
+from scipy.ndimage import median_filter #scientific numpy,has advanced functions
+from sklearn.cluster import KMeans #masking lung ,catageorization of data points
+from skimage import morphology,measure #for image dilation,lines around lungs being eliminated
+import matplotlib.pyplot as plt #graphs
+def preprocess_image(arr): #for filtering out lung part
     arr = np.resize(arr,[512,512])
     mean = np.mean(arr)
     std = np.std(arr)
@@ -18,12 +18,12 @@ def preprocess_image(arr):
     #label_vals = np.unique(labels)
     regions = measure.regionprops(labels)
     targets = []
-    for region in regions:
+    for region in regions: #bounding box used
         Box = region.bbox
         if (Box[0] > 40 and Box[2] < 472 and Box[3] < 475 and Box[2] - Box[0] < 475):
             targets.append(region.label)
     mask = np.zeros([arr.shape[0], arr.shape[1]], dtype=np.int8)
-    for target in targets:
+    for target in targets: #for extracting lung part
         mask = mask + np.where(labels == target, 1, 0)
     masked_img = (mask * arr)
     return masked_img
